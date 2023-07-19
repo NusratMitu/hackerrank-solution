b818b21a893f5e09f99b76d4f2193c938c92c92e
NusratMitu <nusratmitu010@.com>
2023-07-19 17:32:05 +0600

    sock-merchant solution

--- sock-merchant.js
+++ sock-merchant.js
@@ -1,13 +1,25 @@
 const countPair = (a,n) => {
-let count = {}
-let duplicate = []
+// let count = {}
+// let duplicate = []
+// let sum = 0;
+// for (let i of a) {
+//     count[i] = count[i] + 1 || 1 ;
+//     console.log(count);
+//     if(count[i] % 2 === 0)
+//      sum += 1;
+//         console.log({sum});
+// }
+
+// return sum;
+const count = new Map();
 let sum = 0;
-for (let i of a) {
-    count[i] = count[i] + 1 || 1 ;
-    console.log(count);
-    if(count[i] % 2 === 0)
-     sum += 1;
-        console.log({sum});
+
+for (const num of a) {
+  count.set(num, (count.get(num) || 0) + 1);
+  console.log( count.set(num, (count.get(num))));
+  if (count.get(num) % 2 === 0) {
+    sum++;
+  }
 }
 
 return sum;
