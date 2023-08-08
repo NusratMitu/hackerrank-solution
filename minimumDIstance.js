b83780a48ef211a267cfde62fdc778e4abb01b5e
NusratMitu <nusratmitu010@gmail.com>
2023-08-08 09:11:44 +0600

    min distance

--- minimumDIstance.js
+++ minimumDIstance.js
@@ -3,12 +3,31 @@ const matchedItem = (a) => {
     for(let i = 0; i < a.length; i++) {
         for(let j = i+1; j < a.length; j++) {
             if(a[j] === a[i]){
-                console.log(j -i);
+                // console.log(j -i);
                 distance.push(j-i)
             }
+           
+        }
+       
+    }
+    let temp = distance[0];
+       
+        if(distance?.length){
+            for(let i = 0; i < distance?.length; i++) {
             
-        }}
-        return distance
-}
-const a = [7, 1,3,4,1,7]
+                if(distance[i] < temp){
+                    temp = distance[i]
+                }
+              
+             
+            }
+        //    console.log({temp});
+           return temp;
+        }
+        else return -1
+  
+        }
+        
+// const a = [7, 1,3,4]
+const a = [7, 1,3,4, 1, 7]
 console.log(matchedItem(a));
