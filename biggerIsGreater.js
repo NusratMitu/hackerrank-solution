b10e5604af11e442d36922e92ccd1c89a0a814ad
NusratMitu <nusratmitu010@.com>
2023-08-20 17:30:48 +0600

    bigger

--- biggerIsGreater.js
+++ biggerIsGreater.js
@@ -1,6 +1,17 @@
-// const biggerIsGreater = () => {
-//     const 
-// }
+const biggerIsGreater = () => {
+    const charArray = w.split('');
+    const i = charArray.length - 2; 
+    console.log({i});
+    while (i >= 0 && charArray[i] > charArray[i+1]){
+        i--;
+    }
+    if (i === -1) {
+        return "no answer"
+    }
+    }
 
-const w = 'abcd'
-console.log(w.split(''));
+const w = 'abcdfe'
+console.log(biggerIsGreater(w));
+
+
+
