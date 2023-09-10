05f3fa29f30d54c3a0d3f1c0d0ad6d9fe74f1fc0
NusratMitu <nusratmitu010@gmail.com>
2023-09-10 20:54:36 +0600

    bigger is greater

--- biggerIsGreater.js
+++ biggerIsGreater.js
@@ -1,17 +1,29 @@
-const biggerIsGreater = () => {
+const biggerIsGreater = (w) => {
     const charArray = w.split('');
-    const i = charArray.length - 2; 
-    console.log({i});
-    while (i >= 0 && charArray[i] > charArray[i+1]){
+    let i = charArray.length - 2; 
+     // Find the first character that is smaller than the character next to it
+    while (i >= 0 && charArray[i] >= charArray[i+1]){
         i--;
     }
+    // console.log({i});
     if (i === -1) {
-        return "no answer"
+        return "no answer" // It's already the largest possible permutation
     }
+     // Find the smallest character to the right of arr[i] but greater than arr[i]
+    let j =  charArray.length - 1;
+    while(charArray[j] <= charArray[i]){
+        j--;
+    }
+    [charArray[i],charArray[j]] = [ charArray[j], charArray[i] ]
+    const reversedSubArray =  charArray.slice(i+1).reverse();
+    // console.log({reversedSubArray});
+    charArray.splice(i+1, charArray.length - i - 1, ...reversedSubArray)
+    return charArray.join('')
     }
 
-const w = 'abcdfe'
-console.log(biggerIsGreater(w));
 
 
-
+console.log(biggerIsGreater("bdca"));  // ba
+console.log(biggerIsGreater("mnfe"));  // no answer
+console.log(biggerIsGreater("mmfe")); // hegf
+console.log(biggerIsGreater("bb"));
